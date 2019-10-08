133536f89d4cd4fb3ed90f4b3963008116a8bb0c
Emily Harte <emilylharte@gmail.com>
2019-10-08 10:38:05 +0100

    Added public class LCA with algo for determining Lowest Common Ancestor

--- Lowest Common Ancestor/src/LCA.java
+++ Lowest Common Ancestor/src/LCA.java
@@ -10,4 +10,30 @@ class Node {
 }
 
 public class LCA {
+
+    Node root;
+
+    Node findLCA(int node1, int node2) {
+
+        return findLCA(root, node1, node2);
+    }
+
+    Node findLCA(Node node, int node1, int node2) {
+
+        if (node == null) {
+            return null;
+        }
+
+        if (node.data == node1 || node.data == node2) {
+            return node;
+        }
+
+        Node left_LCA = findLCA(node.left, node1, node2);
+        Node right_LCA = findLCA(node.right, node1, node2);
+
+        if (left_LCA != null && right_LCA != null) {
+            return node;
+        }
+        return (left_LCA != null) ? left_LCA : right_LCA;
+    }
 }
