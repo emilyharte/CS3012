2b3b7df23e544e6dd12aa69af1ace8519630cd37
Emily Harte <emilylharte@gmail.com>
2019-10-14 15:38:25 +0100

    Functions to add edges to graph and determine whether edge added creates a cycle

--- Lowest Common Ancestor/src/DAG.java
+++ Lowest Common Ancestor/src/DAG.java
@@ -12,4 +12,33 @@ public class DAG {
         }
     }
 
+    public void addEdge(int v, int w) {
+
+        if (v >= 0 && v < table.length && w >= 0 && w < table.length) {
+            addEdge(table, v, w);
+        }
+    }
+
+    public void addEdge(ArrayList<Integer>[] table, int v, int w) {
+        ArrayList<Integer> list = table[v];
+        if(!list.contains(w)); {
+            list.add(w);
+        }
+        if(containsCycle()) {
+            for(int i =0; i < list.size(); i++) {
+                if(list.get(i) == w) {
+                    list.remove(i);
+                }
+            }
+        }
+    }
+
+    public boolean containsCycle() {
+
+        int[] vertices = new int[table.length];
+
+        return false;
+
+    }
+
 }
